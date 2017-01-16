aac916d1fd7993a6ca3928363a3b6401546d59a4
Salleh Jahaf <salj4215@gmail.com>
2017-01-16 00:35:25 -0500

    Add files via upload

---
+++ Tuition and Fees Calculator.Designer.vb
@@ -0,0 +1,354 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmTFCalculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblTotalFee = New System.Windows.Forms.Label()
+        Me.lblTuition = New System.Windows.Forms.Label()
+        Me.lblTotalDue = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnPrintForm = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtLabFees = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCourseFees = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtCreditHours = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtStudentName = New System.Windows.Forms.TextBox()
+        Me.radDearbornResident = New System.Windows.Forms.RadioButton()
+        Me.radOutOfDistrict = New System.Windows.Forms.RadioButton()
+        Me.radInternational = New System.Windows.Forms.RadioButton()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.prtPrintFormImage = New System.Drawing.Printing.PrintDocument()
+        Me.chkNewStudent = New System.Windows.Forms.CheckBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(406, 193)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(64, 15)
+        Me.Label5.TabIndex = 17
+        Me.Label5.Text = "Total fees: "
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(395, 233)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(75, 15)
+        Me.Label6.TabIndex = 19
+        Me.Label6.Text = "Total tuition:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(410, 271)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(60, 15)
+        Me.Label7.TabIndex = 21
+        Me.Label7.Text = "Total due:"
+        '
+        'lblTotalFee
+        '
+        Me.lblTotalFee.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotalFee.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalFee.Location = New System.Drawing.Point(475, 185)
+        Me.lblTotalFee.Name = "lblTotalFee"
+        Me.lblTotalFee.Size = New System.Drawing.Size(100, 23)
+        Me.lblTotalFee.TabIndex = 18
+        Me.lblTotalFee.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTuition
+        '
+        Me.lblTuition.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTuition.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTuition.Location = New System.Drawing.Point(475, 225)
+        Me.lblTuition.Name = "lblTuition"
+        Me.lblTuition.Size = New System.Drawing.Size(100, 23)
+        Me.lblTuition.TabIndex = 20
+        Me.lblTuition.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTotalDue
+        '
+        Me.lblTotalDue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotalDue.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalDue.Location = New System.Drawing.Point(475, 263)
+        Me.lblTotalDue.Name = "lblTotalDue"
+        Me.lblTotalDue.Size = New System.Drawing.Size(100, 23)
+        Me.lblTotalDue.TabIndex = 22
+        Me.lblTotalDue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(695, 171)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 13
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(695, 218)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 14
+        Me.btnClear.Text = "Clea&r"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnPrintForm
+        '
+        Me.btnPrintForm.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPrintForm.Location = New System.Drawing.Point(695, 263)
+        Me.btnPrintForm.Name = "btnPrintForm"
+        Me.btnPrintForm.Size = New System.Drawing.Size(75, 23)
+        Me.btnPrintForm.TabIndex = 15
+        Me.btnPrintForm.Text = "Print &Form"
+        Me.btnPrintForm.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(695, 313)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 16
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(51, 251)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(105, 15)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Enter course fees:  "
+        '
+        'txtLabFees
+        '
+        Me.txtLabFees.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLabFees.Location = New System.Drawing.Point(162, 284)
+        Me.txtLabFees.Name = "txtLabFees"
+        Me.txtLabFees.Size = New System.Drawing.Size(100, 23)
+        Me.txtLabFees.TabIndex = 12
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 171)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(144, 15)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Enter student's full name: "
+        '
+        'txtCourseFees
+        '
+        Me.txtCourseFees.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCourseFees.Location = New System.Drawing.Point(162, 243)
+        Me.txtCourseFees.Name = "txtCourseFees"
+        Me.txtCourseFees.Size = New System.Drawing.Size(100, 23)
+        Me.txtCourseFees.TabIndex = 10
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(18, 213)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(138, 15)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Enter credit hours taken: "
+        '
+        'txtCreditHours
+        '
+        Me.txtCreditHours.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCreditHours.Location = New System.Drawing.Point(162, 205)
+        Me.txtCreditHours.Name = "txtCreditHours"
+        Me.txtCreditHours.Size = New System.Drawing.Size(100, 23)
+        Me.txtCreditHours.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(70, 292)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(86, 15)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Enter lab fees:  "
+        '
+        'txtStudentName
+        '
+        Me.txtStudentName.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtStudentName.Location = New System.Drawing.Point(162, 166)
+        Me.txtStudentName.Name = "txtStudentName"
+        Me.txtStudentName.Size = New System.Drawing.Size(100, 23)
+        Me.txtStudentName.TabIndex = 6
+        '
+        'radDearbornResident
+        '
+        Me.radDearbornResident.AutoSize = True
+        Me.radDearbornResident.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radDearbornResident.Location = New System.Drawing.Point(319, 39)
+        Me.radDearbornResident.Name = "radDearbornResident"
+        Me.radDearbornResident.Size = New System.Drawing.Size(119, 19)
+        Me.radDearbornResident.TabIndex = 1
+        Me.radDearbornResident.Text = "Dearborn resident"
+        Me.radDearbornResident.UseVisualStyleBackColor = True
+        '
+        'radOutOfDistrict
+        '
+        Me.radOutOfDistrict.AutoSize = True
+        Me.radOutOfDistrict.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radOutOfDistrict.Location = New System.Drawing.Point(319, 64)
+        Me.radOutOfDistrict.Name = "radOutOfDistrict"
+        Me.radOutOfDistrict.Size = New System.Drawing.Size(99, 19)
+        Me.radOutOfDistrict.TabIndex = 2
+        Me.radOutOfDistrict.Text = "Out of District"
+        Me.radOutOfDistrict.UseVisualStyleBackColor = True
+        '
+        'radInternational
+        '
+        Me.radInternational.AutoSize = True
+        Me.radInternational.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radInternational.Location = New System.Drawing.Point(319, 89)
+        Me.radInternational.Name = "radInternational"
+        Me.radInternational.Size = New System.Drawing.Size(164, 19)
+        Me.radInternational.TabIndex = 3
+        Me.radInternational.Text = "Out-of-State/International"
+        Me.radInternational.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(246, 24)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(71, 13)
+        Me.Label8.TabIndex = 0
+        Me.Label8.Text = "Select status:"
+        '
+        'prtPrintFormImage
+        '
+        '
+        'chkNewStudent
+        '
+        Me.chkNewStudent.AutoSize = True
+        Me.chkNewStudent.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkNewStudent.Location = New System.Drawing.Point(564, 40)
+        Me.chkNewStudent.Name = "chkNewStudent"
+        Me.chkNewStudent.Size = New System.Drawing.Size(189, 19)
+        Me.chkNewStudent.TabIndex = 4
+        Me.chkNewStudent.Text = "Are you a new student of HFC?"
+        Me.chkNewStudent.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Assignment_2.My.Resources.Resources.hfc_logo
+        Me.PictureBox1.Location = New System.Drawing.Point(37, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(119, 108)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 23
+        Me.PictureBox1.TabStop = False
+        '
+        'frmTFCalculator
+        '
+        Me.AcceptButton = Me.btnCalculate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(802, 362)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.chkNewStudent)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.radInternational)
+        Me.Controls.Add(Me.radOutOfDistrict)
+        Me.Controls.Add(Me.radDearbornResident)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtLabFees)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtCourseFees)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtCreditHours)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtStudentName)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnPrintForm)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblTotalDue)
+        Me.Controls.Add(Me.lblTuition)
+        Me.Controls.Add(Me.lblTotalFee)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Name = "frmTFCalculator"
+        Me.Text = "HFC Calculator"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents lblTotalFee As System.Windows.Forms.Label
+    Friend WithEvents lblTuition As System.Windows.Forms.Label
+    Friend WithEvents lblTotalDue As System.Windows.Forms.Label
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnPrintForm As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtLabFees As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtCourseFees As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtCreditHours As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtStudentName As System.Windows.Forms.TextBox
+    Friend WithEvents radDearbornResident As System.Windows.Forms.RadioButton
+    Friend WithEvents radOutOfDistrict As System.Windows.Forms.RadioButton
+    Friend WithEvents radInternational As System.Windows.Forms.RadioButton
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents prtPrintFormImage As System.Drawing.Printing.PrintDocument
+    Friend WithEvents chkNewStudent As System.Windows.Forms.CheckBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Tuition and Fees Calculator.vb
@@ -0,0 +1,150 @@
+﻿'HFC Calculator application
+'Program that calculates tuition and total fees of students attending HFC
+'Developed by Salleh Jahaf
+'Version 1.0
+'Updated on 10/08/2015
+Option Strict On
+
+Public Class frmTFCalculator
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub lblTotalFee_Click(sender As Object, e As EventArgs) Handles lblTotalFee.Click
+
+    End Sub
+    'Exit Button
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Close the application when user clicks exit
+        Me.Close()
+
+    End Sub
+    'Calculate button
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'Declare constants
+        Const intADMISSION_APPLICATION_FEE As Integer = 30
+        Const intREGISTRATION_FEE As Integer = 46
+        Const intINFRASTRUCTURE_FEE As Integer = 45
+        Const intDEARBORN_TUITION_RATE As Integer = 92
+        Const intOUT_OF_DISTRICT_TUITION_RATE As Integer = 158
+        Const intOUT_OF_STATE_INTERNATIONAL_RATE As Integer = 225
+        Const intSERVICE_FEE_RATE As Integer = 15
+        Const intTECH_INVESTMENT_FEE_RATE As Integer = 2
+
+        'Declare variables
+        Dim intCreditHours As Integer
+        Dim intCourseFees As Integer
+        Dim intLabFees As Integer
+        Dim intTotalFees As Integer
+        Dim intTuitionTotal As Integer
+        Dim intFinalTotal As Integer
+        Dim intTuitionRate As Integer
+
+        'Convert input into numbers, then assign to variables
+        Integer.TryParse(txtCourseFees.Text, intCourseFees)
+        Integer.TryParse(txtCreditHours.Text, intCreditHours)
+        Integer.TryParse(txtLabFees.Text, intLabFees)
+
+        'Code message box to show if no radio buttons are selected by user
+        If radDearbornResident.Checked = False And radOutOfDistrict.Checked = False And radInternational.Checked = False Then
+            MessageBox.Show("Please choose your status", "Henry Ford College", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Exit Sub
+        End If
+        'Code message box to show if user name is not entered
+        If txtStudentName.Text = String.Empty Then
+            MessageBox.Show("Please enter your name", "Henry Ford College", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            txtStudentName.Focus()
+            Exit Sub
+        End If
+        'Code message box if invalid number of credit hours is entered
+        If intCreditHours < 2 Or intCreditHours > 18 Then
+            MessageBox.Show("Please enter correct number of credit hours. Only 2 to 18 credit hours are allowed", "Henry Ford College", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            txtCreditHours.Focus()
+            txtCreditHours.SelectAll()
+        End If
+        'Assign tuition rate based on radio button selection
+        If radDearbornResident.Checked Then
+            intTuitionRate = intDEARBORN_TUITION_RATE
+        ElseIf radOutOfDistrict.Checked Then
+            intTuitionRate = intOUT_OF_DISTRICT_TUITION_RATE
+        ElseIf radInternational.Checked Then
+            intTuitionRate = intOUT_OF_STATE_INTERNATIONAL_RATE
+        End If
+        'Calculate total fee and other fees
+        intTotalFees = intCreditHours * intSERVICE_FEE_RATE + intCreditHours * intTECH_INVESTMENT_FEE_RATE + intCourseFees + intLabFees + intREGISTRATION_FEE + intINFRASTRUCTURE_FEE
+        'Add application fee to total fee if new student checkbox is checked
+        If chkNewStudent.Checked = True Then
+            intTotalFees = intTotalFees + intADMISSION_APPLICATION_FEE
+        End If
+        intTuitionTotal = intCreditHours * intTuitionRate
+        intFinalTotal = intTotalFees + intTuitionTotal
+        'Send totals to label boxes
+        lblTotalFee.Text = intTotalFees.ToString("N2")
+        lblTuition.Text = intTuitionTotal.ToString("N2")
+        lblTotalDue.Text = intFinalTotal.ToString("C2")
+
+
+    End Sub
+
+   
+
+
+    'Code so if any characters other than numbers and backspace are not inputed into text boxes by user
+    Private Sub Cancel_Keys(sender As Object, e As KeyPressEventArgs) Handles txtCourseFees.KeyPress,
+        txtCreditHours.KeyPress, txtLabFees.KeyPress
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+    End Sub
+    'Clear all outputs when text within text boxes are changed by user
+    Private Sub Clear_Labels(sender As Object, e As EventArgs) Handles txtCourseFees.TextChanged, txtCreditHours.TextChanged,
+        txtLabFees.TextChanged, txtStudentName.TextChanged, radDearbornResident.Click, radOutOfDistrict.Click, radInternational.Click,
+        chkNewStudent.CheckedChanged
+
+        lblTuition.Text = String.Empty
+        lblTotalFee.Text = String.Empty
+        lblTotalDue.Text = String.Empty
+
+    End Sub
+    'Clear button
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        radDearbornResident.Checked = False
+        radOutOfDistrict.Checked = False
+        radInternational.Checked = False
+        chkNewStudent.Checked = False
+        txtStudentName.Text = String.Empty
+        txtCreditHours.Text = String.Empty
+        txtCourseFees.Text = String.Empty
+        txtLabFees.Text = String.Empty
+        lblTuition.Text = String.Empty
+        lblTotalFee.Text = String.Empty
+        lblTotalDue.Text = String.Empty
+    End Sub
+    'Print button
+    Dim memoryImage As Bitmap
+    Private Sub CaptureScreen()
+        'this code will create a graphics image in memory and copy this form’s image to it
+
+        Dim myGraphics As Graphics = Me.CreateGraphics()
+        Dim s As Size = Me.Size
+        memoryImage = New Bitmap(s.Width, s.Height, myGraphics)
+        Dim memoryGraphics As Graphics = Graphics.FromImage(memoryImage)
+        btnCalculate.visible = False
+        btnClear.visible = False
+        btnPrintForm.visible = False
+        btnExit.visible = False
+        memoryGraphics.CopyFromScreen(Me.Location.X, Me.Location.Y, 0, 0, s)
+        btnCalculate.visible = True
+        btnClear.visible = True
+        btnPrintForm.visible = True
+        btnExit.visible = True
+    End Sub
+    Private Sub prtPrintFormImage_PrintPage(ByVal sender As System.Object, ByVal e As System.Drawing.Printing.PrintPageEventArgs) Handles prtPrintFormImage.PrintPage
+        e.Graphics.DrawImage(memoryImage, 0, 0)
+    End Sub
+    Private Sub btnPrintForm_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPrintForm.Click
+        CaptureScreen()
+        prtPrintFormImage.Print()
+    End Sub
+End Class
